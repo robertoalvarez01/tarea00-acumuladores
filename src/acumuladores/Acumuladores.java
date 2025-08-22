3049a603db8fc274b35d7259bd74dbcde6241428
Roberto Alvarez <robertogonzaloalvarez01@gmail.com>
2025-08-21 21:16:27 -0300

    Update Acumuladores.java

--- src/acumuladores/Acumuladores.java
+++ src/acumuladores/Acumuladores.java
@@ -14,29 +14,19 @@ public class Acumuladores {
 	 * @return
 	 */
 	public boolean todosMultiplosEnAlgunaFila(int[][] mat, int num) {
-
-		
-		if(mat.length == 0 || num < 0)return false;
-		
-		boolean acumulador = true;
-		
-		for(var i = 0; i< mat.length; i++ ) {
-			
-			acumulador = true;
-			
-			int[] fila = mat[i];
-			
-			for(var j = 0; j<fila.length ; j++) {
-				
-				acumulador = acumulador && (j % num == 0);
-				
+		if (mat.length == 0 || num <= 0) return false;
+	
+		for (int i = 0; i < mat.length; i++) {
+			boolean filaValida = true;
+			for (int j = 0; j < mat[i].length; j++) {
+				if (mat[i][j] % num != 0) {
+					filaValida = false;
+					break;
+				}
 			}
-			if(acumulador) break;
+			if (filaValida) return true;
 		}
-		
-		return acumulador;
-		
-		
+		return false;
 	}
 	
 	/**
@@ -50,8 +40,23 @@ public class Acumuladores {
 	 * @param mat2
 	 * @return
 	 */
-	public boolean hayInterseccionPorFila(int[][] mat1, int[][]mat2) { 
-		throw new RuntimeException("Metodo no implementado aun!!!");
+	public boolean hayInterseccionPorFila(int[][] mat1, int[][] mat2) {
+		if (mat1.length == 0 || mat2.length == 0 || mat1.length != mat2.length) return false;
+	
+		for (int i = 0; i < mat1.length; i++) {
+			boolean hayInterseccion = false;
+			for (int a : mat1[i]) {
+				for (int b : mat2[i]) {
+					if (a == b) {
+						hayInterseccion = true;
+						break;
+					}
+				}
+				if (hayInterseccion) break;
+			}
+			if (!hayInterseccion) return false;
+		}
+		return true;
 	}
 	
 	/**
@@ -66,8 +71,23 @@ public class Acumuladores {
 	 * @param nColum
 	 * @return
 	 */
-	public boolean algunaFilaSumaMasQueLaColumna(int[][] mat, int nColum) { 
-		throw new RuntimeException("Metodo no implementado aun!!!");
+	public boolean algunaFilaSumaMasQueLaColumna(int[][] mat, int nColum) {
+		if (mat.length == 0 || mat[0].length <= nColum || nColum < 0) return false;
+	
+		int sumaColumna = 0;
+		for (int i = 0; i < mat.length; i++) {
+			sumaColumna += mat[i][nColum];
+		}
+	
+		for (int i = 0; i < mat.length; i++) {
+			int sumaFila = 0;
+			for (int j = 0; j < mat[i].length; j++) {
+				sumaFila += mat[i][j];
+			}
+			if (sumaFila > sumaColumna) return true;
+		}
+	
+		return false;
 	}
 	
 	/**
@@ -81,7 +101,24 @@ public class Acumuladores {
 	 * @param mat2
 	 * @return
 	 */
-	public boolean hayInterseccionPorColumna(int[][] mat1, int[][]mat2) { 
-		throw new RuntimeException("Metodo no implementado aun!!!");
+	public boolean hayInterseccionPorColumna(int[][] mat1, int[][] mat2) {
+		if (mat1.length == 0 || mat2.length == 0 || mat1[0].length != mat2[0].length) return false;
+	
+		int columnas = mat1[0].length;
+		for (int j = 0; j < columnas; j++) {
+			boolean hayInterseccion = false;
+			for (int i1 = 0; i1 < mat1.length; i1++) {
+				for (int i2 = 0; i2 < mat2.length; i2++) {
+					if (mat1[i1][j] == mat2[i2][j]) {
+						hayInterseccion = true;
+						break;
+					}
+				}
+				if (hayInterseccion) break;
+			}
+			if (!hayInterseccion) return false;
+		}
+	
+		return true;
 	}
 }
